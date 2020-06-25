e86dd9d799dc5b33defe26750bcfc65655fd741d
Kaito0208 <minhtranplus.0208@gmail.com>
2020-06-25 22:53:05 +0700

    Adding Header Component and Link To Home

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
 import React from "react";
 import "./App.css";
-import { Route, Link, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
-const TopicsList = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1> Topic List Page</h1>
-      <Link to={`${props.match.url}/13`}>To Topic 13</Link>
-      <Link to={`${props.match.url}/14`}>To Topic 14</Link>
-      <Link to={`${props.match.url}/15`}>To Topic 15</Link>
-    </div>
-  )
-}
-const TopicDetail = (props) => {
-  return (
-    <div>
-      <button onClick={() => props.history.push('/topics') }>click</button>
-      <h1> Topic Detail Page: {props.match.params.topicId}</h1>
-    </div>
-  )
-}
+// const TopicsList = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1> Topic List Page</h1>
+//       <Link to={`${props.match.url}/13`}>To Topic 13</Link>
+//       <Link to={`${props.match.url}/14`}>To Topic 14</Link>
+//       <Link to={`${props.match.url}/15`}>To Topic 15</Link>
+//     </div>
+//   )
+// }
+// const TopicDetail = (props) => {
+//   return (
+//     <div>
+//       <button onClick={() => props.history.push('/topics') }>click</button>
+//       <h1> Topic Detail Page: {props.match.params.topicId}</h1>
+//     </div>
+//   )
+// }
 
 function App() {
   return (
     <div className="wrapper">
+      <Header />
       <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/topics" component={TopicsList} />
-      <Route exact path="/topics/:topicId" component={TopicDetail} />
+      {/* <Route exact path="/topics" component={TopicsList} />
+      <Route exact path="/topics/:topicId" component={TopicDetail} /> */}
       <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
