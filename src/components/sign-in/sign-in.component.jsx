d929795dc8de043a4a2a723d37d480dfd8dc37fe
Kaito0208 <minhtranplus.0208@gmail.com>
2020-07-01 00:21:27 +0700

    fix Sign in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './sign-in.styles.scss';
-import classes from '*.module.css';
 
 class SignIn extends React.Component{
     constructor(props) {
@@ -14,17 +13,23 @@ class SignIn extends React.Component{
         event.preventDefault();
         this.setState({email: '', password: ''})
     }
+
+    handleChange = event => {
+        const {value, name} = event.target;
+        this.setState({[name]: value})
+    }
+
     render() {
         return(
            <div className='sign-in'>
              <h2> I already have an account </h2>
              <span> Sign in with your email and password</span>
              <form onSubmit={this.handleSubmit}>
-                 <input name='email' type='email' value={this.state.email} required />
+                 <input name='email' type='email' value={this.state.email} onChange={this.handleChange} required />
                  <label> Email</label>
-                 <input name='password' type='passwrord' value= {this.state.email} required />
+                 <input name='password' type='passwrord' value= {this.state.email} onChange={this.handleChange} required />
                  <label>Email</label>
-                 <input type='submit' value='Submit Form'
+                 <input type='submit' value='Submit Form' />
              </form>
            </div>
         )
